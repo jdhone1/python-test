5b5aab66359d9ec8144f564dbbeca336e2ac5777
Gray Hall <sfjcghbw9191@hotmail.com>
2026-01-19 11:01:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-# Last updated: 2026-01-16 05:06:55
+# Last updated: 2026-01-19 11:01:04
 
 import datetimedef get_current_time():    now = datetime.datetime.now()    print(f"Current time: {now.strftime('%Y-%m-%d %H:%M:%S')}")    return nowif __name__ == "__main__":    get_current_time()
